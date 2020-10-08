6d22a835405bd71af485643967ed16dc4b7e806f
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-10-08 17:35:23 +0500

    Update YahooAPI.java

--- src/main/java/API/YahooAPI.java
+++ src/main/java/API/YahooAPI.java
@@ -1,25 +1,33 @@
 package API;
 
-import com.fasterxml.jackson.databind.type.MapLikeType;
-import com.google.gson.Gson;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap;
+
 
 public class YahooAPI {
-    private static List<String> stocks = Arrays.asList("AMD", "IBM", "AAPl");
-    public static Object getQuotes() throws UnirestException {
+    private static final String stocks = String.join("%2C", Arrays.asList("AMD", "IBM", "AAPl"));
+
+    public static HashMap<String, Double> getQuotes() throws UnirestException {
         var response = Unirest.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes")
                 .queryString("symbols", stocks)
-                .queryString("region","US")
+                .queryString("region", "US")
                 .header("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-                .asJson().toString();
-        
-        return response;
+                .header("x-rapidapi-key", "")//paste token
+                .asJson()
+                .getBody()
+                .getObject()
+                .getJSONObject("quoteResponse")
+                .getJSONArray("result");
+
+        var prices = new HashMap<String, Double>();
+        for (int i = 0; i < response.length(); i++) {
+            var stock = response.getJSONObject(i);
+            prices.put(stock.get("longName").toString(), (Double) stock.get("postMarketPrice"));
+        }
+
+        return prices;
     }
 }
